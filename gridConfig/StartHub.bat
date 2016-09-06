53c974c7980271d8206ec8c1ec980275e7754035
EMEA\KarkareR <rakesh.karkare@travelex.com>
2016-09-06 19:23:17 +0530

    --grid Config commit Changes

---
+++ gridConfig/StartHub.bat
@@ -0,0 +1,4 @@
+cd\
+c:
+cd C:\NAMProject\MAVENNAMProject\gridConfig
+java -jar selenium-server-standalone-2.45.0.jar -role hub -timeout 900 -browserTimeout 1800 -port 4444
---
+++ gridConfig/StartNode.bat
@@ -0,0 +1,4 @@
+cd\
+c:
+cd C:\NAMProject\MAVENNAMProject\gridConfig
+java -jar selenium-server-standalone-2.45.0.jar -role webdriver -hub http://IN-MU-LT-J27432:4444/grid/register -Dwebdriver.chrome.driver=chromedriver.exe  -nodeConfig config.json
