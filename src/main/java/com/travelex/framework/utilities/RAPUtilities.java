b8c1069344326a86cc44200017c189a0fe87dd35
EMEA\reddyP <praveen.reddy@travelex.com>
2017-05-29 18:43:27 +0530

    Functions Creation For Pluto

--- src/main/java/com/travelex/framework/utilities/RAPUtilities.java
+++ src/main/java/com/travelex/framework/utilities/RAPUtilities.java
@@ -2,10 +2,8 @@ package com.travelex.framework.utilities;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLDataException;
 import java.sql.SQLException;
-import java.sql.Statement;
+
 
 public class RAPUtilities {	
 	
